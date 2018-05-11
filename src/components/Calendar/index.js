6752e6642015e9d8ce2150c4c641459af5ce3849
yyd <609825025@qq.com>
2018-05-11 10:37:07 +0800

    增加新变量

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -18,7 +18,28 @@ export default class Calendar extends React.Component{
     render(){
         return(
             <div className="Calendar">
+                <div className="CalendarSelect">
+                    <i className="iconfont icon-icon1"></i>
+                    <span>选择开始时间</span>
+                    <i className="iconfont icon-xingzhuang4"></i>
+                    <span>选择结束时间</span>
+                </div>
 
+                <div className="CalendarOption">
+                    <div className="CalendarTitle">
+                        <span>2017-06-07 09:49:49</span>
+                        <i className="iconfont icon-xingzhuang4"></i>
+                        <span>2017-06-07 09:49:49</span>
+                    </div>
+
+                    <div className="CalendarMain">
+
+                    </div>
+
+                    <div className="CalendarEnd">
+
+                    </div>
+                </div>
             </div>
         )
     }
