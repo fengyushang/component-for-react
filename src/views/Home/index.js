7fb0812d36b8c07f5632fadbb66c9c87f6124ca3
zhangy <zhangy@htrader.cn>
2018-05-14 15:05:20 +0800

    panel

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'components/style/main.less';
 import Button from 'components/Button';
-import {message, Input, Pagination, Row, Col} from '../../components';
+import {message, Input, Pagination, Row, Col, Panel} from '../../components';
 import Select, {InputSelect} from 'components/Select';
 import Tipsy from 'components/Tipsy';
 import {autobind} from 'core-decorators';
@@ -25,22 +25,22 @@ export default class Home extends React.Component {
     render() {
         const {input1, total, pageSize, current, demo, demo2} = this.state;
         return <div className='home-page'>
-            <div className='rightContent'>
+            <Panel title='按钮' className='rightContent'>
                 <Button label="确定" sureBtn/>
                 <Button label="禁止" sureBtn disabled/>
                 <Button label="取消" cancelBtn/>
                 <Button label="禁止" cancelBtn disabled/>
-            </div>
-            <div>
+            </Panel>
+            <Panel title='input输入框'>
                 <Input name='input1'
                        value={input1}
                        onChange={(name, value) => this.setState({input1: value})}
                        placeholder='提示信息'
                        unit='美元'
                 />
-            </div>
-            <div className="clearfix">
-                <div className="fl">
+            </Panel>
+            <Panel title='select'>
+                <div>
                     常规下拉框：
                     <Select
                         name="demo"
@@ -59,7 +59,7 @@ export default class Home extends React.Component {
                         }
                     />
                 </div>
-                <div className="fl" style={{'marginLeft': '20px'}}>
+                <div style={{'marginTop': '20px'}}>
                     可输入下拉框：
                     <InputSelect
                         name="demo2"
@@ -90,8 +90,8 @@ export default class Home extends React.Component {
                     />
 
                 </div>
-            </div>
-            <div style={{'clear': 'both'}}>
+            </Panel>
+            <Panel title='分页'>
                 <Pagination total={total}
                             pageSize={pageSize}
                             current={current}
@@ -103,35 +103,38 @@ export default class Home extends React.Component {
                         next: '下一页',
                     }}*/
                             border={false}/>
-            </div>
-            <div>
-                <div>message信息</div>
+            </Panel>
+            <Panel title='message信息'>
                 <Button label="success" sureBtn onClick={() => message.success('success')}/>
                 <Button label="info" sureBtn onClick={() => message.info('info')}/>
                 <Button label="warning" sureBtn onClick={() => message.warning('warning')}/>
                 <Button label="error" sureBtn onClick={() => message.error('error')}/>
-            </div>
-            <div style={{paddingLeft: '300px'}}>
-                <div>悬浮提示框</div>
-                <Tipsy tipsyText='你瞅啥？瞅你咋地！'/>
-                <Tipsy tipsyText='波棱盖卡马路牙子上，卡秃噜皮了' placement='right'/>
-                <Tipsy
-                    tipsyText='有一种回答叫嗯哪,有一种解决叫咋整,　有一种浪费叫霍霍，有一种重复叫磨叽，有一种状况叫毛楞,有一种面貌叫磕碜，有一种讨厌叫咯应,有一种观察叫撒漠,有一种掩护叫打狼,有一种为人叫得瑟,'
-                    placement='bottom'/>
-                <Tipsy tipsyText='有一种疑问叫噶哈，有一种习惯叫埋汰，有一种聊天叫唠嗑，有一种速度叫麻溜，有一种愤怒叫急眼，有一种喜欢叫稀罕，有一种厉害叫尿性，有一种傻叫得儿呵，有一种心情叫憋屈，'
-                       placement='left'/>
-            </div>
-            <Row gutter={8} className='row-test'>
-                <Col span={3}>
-                    <div>1</div>
-                </Col>
-                <Col span={6}>
-                    <div>2</div>
-                </Col>
-                <Col span={3}>
-                    <div>3</div>
-                </Col>
-            </Row>
+            </Panel>
+            <Panel title='悬浮提示框'>
+                <div style={{paddingLeft: '300px'}}>
+                    <div>悬浮提示框</div>
+                    <Tipsy tipsyText='你瞅啥？瞅你咋地！'/>
+                    <Tipsy tipsyText='波棱盖卡马路牙子上，卡秃噜皮了' placement='right'/>
+                    <Tipsy
+                        tipsyText='有一种回答叫嗯哪,有一种解决叫咋整,　有一种浪费叫霍霍，有一种重复叫磨叽，有一种状况叫毛楞,有一种面貌叫磕碜，有一种讨厌叫咯应,有一种观察叫撒漠,有一种掩护叫打狼,有一种为人叫得瑟,'
+                        placement='bottom'/>
+                    <Tipsy tipsyText='有一种疑问叫噶哈，有一种习惯叫埋汰，有一种聊天叫唠嗑，有一种速度叫麻溜，有一种愤怒叫急眼，有一种喜欢叫稀罕，有一种厉害叫尿性，有一种傻叫得儿呵，有一种心情叫憋屈，'
+                           placement='left'/>
+                </div>
+            </Panel>
+            <Panel title='栅栏'>
+                <Row gutter={8} className='row-test'>
+                    <Col span={3}>
+                        <div>1</div>
+                    </Col>
+                    <Col span={6}>
+                        <div>2</div>
+                    </Col>
+                    <Col span={3}>
+                        <div>3</div>
+                    </Col>
+                </Row>
+            </Panel>
         </div>
     }
 }
