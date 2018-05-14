62b91fbc08f2e7c4f054945d90bd688d6fc90fa8
sangzhenlin <17010282446@163.com>
2018-05-14 11:06:55 +0800

    悬浮床提示

---
+++ src/components/Tipsy/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {autobind} from 'core-decorators';
+import PropTypes from 'prop-types';
+import IconFont from '../IconFont';
+import './style.less';
+@autobind
+export default class Tipsy extends React.Component {
+    static props={
+        className:PropTypes.string,
+        tipsyText:PropTypes.string,
+        placement:PropTypes.string
+    };
+    static defaultProps={
+        placement:'top'
+    };
+    render() {
+        const { className,tipsyText,placement } = this.props;
+        return (
+            <span className={ (className?className:'')+' tipsy '+(placement?placement:'') }>
+                <IconFont name='ICON'/>
+                <div className='tipsy-text'>{ tipsyText }</div>
+            </span>
+        )
+    }
+}
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -5,6 +5,7 @@ import Input from 'components/Input';
 import Pagination from 'components/Pagination';
 import {success, info, warning, error} from 'components/Message';
 import Select,{InputSelect} from 'components/Select';
+import Tipsy from 'components/Tipsy';
 import {autobind} from 'core-decorators';
 import './style.less';
 
@@ -97,6 +98,13 @@ export default class Home extends React.Component {
                 <Button label="info" sureBtn onClick={()=>info('info')}/>
                 <Button label="warning" sureBtn onClick={()=>warning('warning')}/>
                 <Button label="error" sureBtn onClick={()=>error('error')}/>
+            </div>
+            <div style={{paddingLeft:'300px'}}>
+                <div>悬浮提示框</div>
+                <Tipsy tipsyText='你瞅啥？瞅你咋地！'/>
+                <Tipsy tipsyText='波棱盖卡马路牙子上，卡秃噜皮了' placement='right'/>
+                <Tipsy tipsyText='有一种回答叫嗯哪,有一种解决叫咋整,　有一种浪费叫霍霍，有一种重复叫磨叽，有一种状况叫毛楞,有一种面貌叫磕碜，有一种讨厌叫咯应,有一种观察叫撒漠,有一种掩护叫打狼,有一种为人叫得瑟,' placement='bottom'/>
+                <Tipsy tipsyText='有一种疑问叫噶哈，有一种习惯叫埋汰，有一种聊天叫唠嗑，有一种速度叫麻溜，有一种愤怒叫急眼，有一种喜欢叫稀罕，有一种厉害叫尿性，有一种傻叫得儿呵，有一种心情叫憋屈，' placement='left'/>
             </div>
         </div>
     }
