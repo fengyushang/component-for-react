4e172b68d23b74ae9e875022e6200b39fa64f2c4
liur <liur@demomove.cn>
2018-05-15 11:08:01 +0800

    sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,41 +1,44 @@
 import React,{Component} from 'react';
 import PARAMS from './config.js';
+ 
 import './style.less';
 
 export default class SideBar extends Component{
-
-    getCnode(){
-
+    state = {
+        pIndex:0,
+        cIndex:0
+    }
+    getCnode(items){
+        const {cIndex} = this.state;
+        const li = items && items.map((item,idx)=>{
+            const cls = idx == cIndex ? 'hover' : ''
+            return <li key={idx} className={cls} onClick={()=>this.changeCindex(idx)}>{item.label}</li>
+        });
+        return li;
+    }
+    changePindex(idx){
+        console.log(1111);
+        this.setState({pIndex:idx,cIndex:0});
+    }
+    changeCindex(idx){
+        console.log(idx);
+        this.setState({cIndex:idx});
     }
     render(){
+        const {pIndex} = this.state;
         return (
             <div className="sideBar">
                 <div className="treeDom">
-                    <ul>
-                        {/* {
+                    <ul className="cUl">
+                        {
                             PARAMS.map((item,idx)=>{
-                                const cNode = item.cNode;
-                                return <li key={idx}>
-                                    <div className="parentNode">{item.pNode.label}</div>
+                                const cNode = this.getCnode(item.cNode);
+                                const cls = idx == pIndex ? 'active' : '';
+                                return <li key={idx} className={cls} >
+                                    <div className="parentNode" onClick={()=>this.changePindex(idx)}>{item.pNode.label}</div><ul>{cNode}</ul>
                                 </li>
                             })
-                        } */}
-
-                        <li className="active">
-                            <div className="parentNode">商品信息</div>
-                            <ul>
-                                <li className="hover">商品列表</li>
-                                <li>基础配置</li>
-                                <li>板块设置</li>
-                                <li>策略设置</li>
-                                <li>服务器配置</li>
-                            </ul>
-                        </li>
-                        <li>用户信息</li>
-                        <li>资金信息</li>
-                        <li>订单信息</li>
-                        <li>运营信息</li>
-                        <li>系统配置</li>
+                        }
                     </ul>
                 </div>
             </div>
